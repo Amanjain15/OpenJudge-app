1d543a911754b8d0a11de0672c23c0c3994895d3
aditya <aditya999123@gmail.com>
2017-12-29 22:17:36 +0530

    router configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { PageNotFoundComponent } from  './NotFound.component' 
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [ 
+   { path: 'login', component: LoginComponent }, 
+   { path: '', component: HomeComponent}
+   // { path: 'Inventory', component: AppInventory }, 
+   // { path: '**', component: PageNotFoundComponent } 
+];  
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
